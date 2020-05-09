3f3ea979e0f2bafb8516d7bb257083033e51493a
Louis <louis.duretete@gmail.com>
2020-05-09 17:39:46 +0200

    Insertion logo

--- app/src/main/java/com/example/tp1_app1/MainActivity.java
+++ app/src/main/java/com/example/tp1_app1/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.tp1_app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Categorie";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"OnCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"OnStart");
+    }
+
 }
